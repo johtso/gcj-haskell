28d85dd2cabab31bdf7cd8cd3a61ed29b68ee6ea
Johannes <johtso@gmail.com>
2014-03-01 09:18:18 +0000

    Add tests

---
+++ 2013/qualification/A/Tests.hs
@@ -0,0 +1,31 @@
+import Test.HUnit
+
+import Main
+
+
+makeTest :: (Eq b, Show b) => (a -> b) -> (a, b) -> Test
+makeTest f (a, b) = TestCase (assertEqual "" b (f a))
+
+makeTests :: (Eq b, Show b) => (a -> b) -> [(a, b)] -> Test
+makeTests f caseData = TestList $ map (makeTest f) caseData
+
+pathStatusTests :: Test
+pathStatusTests = makeTests pathStatus [
+    ([Empty, Empty, Empty],       Unfinished),
+    ([Empty, Piece X, Empty],     Unfinished),
+    ([Piece X, Piece X, Piece X], Win X)
+    ]
+
+finalGameStatusTests :: Test
+finalGameStatusTests = makeTests finalGameStatus [
+    ([Win X, Unfinished],      Win X),
+    ([Win X, Win O],           Win X),
+    ([Unfinished, Unfinished], Unfinished),
+    ([Win X, Draw],            Win X)
+    ]
+
+allTests :: Test
+allTests = TestList [pathStatusTests, finalGameStatusTests]
+
+main :: IO Counts
+main = runTestTT allTests
